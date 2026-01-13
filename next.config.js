f46c5008ccefdf1cc8df8902bb42317ca562dba2
Salil Samdarshy <52833987+salil1993@users.noreply.github.com>
2026-01-13 23:38:24 +0530

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000',
+  },
+};
+
+module.exports = nextConfig;
